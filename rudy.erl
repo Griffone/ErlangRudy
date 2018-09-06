f2ab6c8b179a7281bfe411951df2bef337a0520a
Griffone <thegriffones@gmail.com>
2018-09-06 14:14:11 +0200

    Improvements to the server.
    Parallelized request handling.
    Imlemented control flow.

--- rudy.erl
+++ rudy.erl
@@ -5,25 +5,42 @@
 -import(http, [parse_request/1, ok/1]).
 
 init(Port) ->
+    spawn(fun() -> rudy(Port) end).
+
+rudy(Port) ->
     Options = [list, {active, false}, {reuseaddr, true}],
     case gen_tcp:listen(Port, Options) of
         {ok, Server} ->
+            Parent = self(),
+            spawn_link(fun() -> acceptor(Server, Parent) end),
+            {ok, ListenPort} = inet:port(Server),
+            io:format("Listening on port ~w.~n", [ListenPort]),
             handler(Server),
-            gen_tcp:close(Server),
-            ok;
+            gen_tcp:close(Server);
         {error, Error} ->
             print_error(Error)
     end.
 
-handler(Server) ->
+handler(Acceptor) ->
+    receive
+        {new_connection, Connection} ->
+            spawn(fun() -> request(Connection) end);
+        stop ->
+            exit(Acceptor, kill);
+        Strange ->
+            io:format("Handler received strange message:~n~w~n", [Strange])
+    end,
+    handler(Acceptor).
+
+acceptor(Server, Parent) ->
     case gen_tcp:accept(Server) of
         {ok, Connection} ->
-            % TODO: increase throughput
-            request(Connection),
-            handler(Server);
-        {error, Error} ->
-            print_error(Error)
-    end.
+            ok = gen_tcp:controlling_process(Connection, Parent),
+            Parent ! {new_connection, Connection};
+        Other ->
+            Parent ! Other
+    end,
+    acceptor(Server, Parent).
     
 request(Connection) ->
     Recv = gen_tcp:recv(Connection, 0),
--- test.erl
+++ test.erl
@@ -1,18 +1,68 @@
 -module(test).
 
--export([benchmark/2]).
+-export([benchmark/1, benchmark/2, sequential/3, parallel/3]).
 
 %-import(http, [get/1]).
 
+benchmark(Port) ->
+    benchmark({127, 0, 0, 1}, Port).
+
 benchmark(Host, Port) ->
-    Start = erlang:system_time(micro_seconds),
-    run(100, Host, Port),
-    Finish = erlang:system_time(micro_seconds),
+    Num = 100,
+    io:format("Running ~w benchmark requests.~n", [Num]),
+    SeqTime = sequential(Num, Host, Port),
+    io:format("~w sequential requests took ~wms (~.2fms per request on avereage).~n", [Num, SeqTime, SeqTime / Num]),
+    {ParTime, ErrNum} = parallel(Num, Host, Port),
+    if
+        ErrNum == 0 ->
+            io:format("~w parallel requests took ~wms (~.2fms per request on avereage).~n", [Num, ParTime, ParTime / Num]);
+        true ->
+            io:format("~w parallel requests took ~wms, of which ~w failed (~.2fms per sucessful request).~n", [Num, ParTime, ErrNum, ParTime / (Num - ErrNum)])
+    end.
+
+sequential(N, Host, Port) ->
+    Start = erlang:system_time(milli_seconds),
+    run(N, Host, Port),
+    Finish = erlang:system_time(milli_seconds),
     Finish - Start.
+
+parallel(N, Host, Port) ->
+    Start = erlang:system_time(milli_seconds),
+    ErrNum = prun(N, Host, Port),
+    Finish = erlang:system_time(milli_seconds),
+    {Finish - Start, ErrNum}.
+
+prun(N, Host, Port) ->
+    plaunch(N, Host, Port),
+    pwait(N, 0).
+
+plaunch(N, Host, Port) ->
+    if
+        N < 1 ->
+            ok;
+        true ->
+            Pid = self(),
+            spawn(fun() -> Pid ! request(Host, Port) end),
+            timer:sleep(1),
+            plaunch(N - 1, Host, Port)
+    end.
+
+pwait(N, EN) ->
+    if
+        N < 1 ->
+            EN;
+        true ->
+            receive
+                {error, _} ->
+                    pwait(N - 1, EN + 1);
+                ok ->
+                    pwait(N - 1, EN)
+            end
+    end.
 
 run(N, Host, Port) ->
     if
-        N == 0 ->
+        N < 1 ->
             ok;
         true ->
             request(Host, Port),
@@ -21,13 +71,17 @@ run(N, Host, Port) ->
 
 request(Host, Port) ->
     Options = [list, {active, false}, {reuseaddr, true}],
-    {ok, Connection} = gen_tcp:connect(Host, Port, Options),
-    gen_tcp:send(Connection, http:get("foo")),
-    Recv = gen_tcp:recv(Connection, 0),
-    case Recv of
-        {ok, _} ->
-            ok;
+    case gen_tcp:connect(Host, Port, Options) of
+        {ok, Connection} ->
+            gen_tcp:send(Connection, http:get("foo")),
+            Recv = gen_tcp:recv(Connection, 0),
+            gen_tcp:close(Connection),
+            case Recv of
+                {ok, _} ->
+                    ok;
+                {error, Error} ->
+                    {error, Error}
+            end;
         {error, Error} ->
-            io:format("Test error:~n~w~n", [Error])
-    end,
-    gen_tcp:close(Connection).
+            {error, Error}
+    end.
